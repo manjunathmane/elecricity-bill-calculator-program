0f4493f2bc2919ad31d4e099d49c7d90c37fd303
manjunathmane <manjunathmane38@gmail.com>
2025-10-06 17:49:28 +0530

    SAVE

--- electric_bill.py
+++ electric_bill.py
@@ -2,3 +2,12 @@ units=int(input("Enter the number of units consumed:"))
 total=units*5
 print("Units Consumed:₹",units)
 print("Total Bill:₹",{total})
+disc=total*0.10
+final1=total   #without discount
+final2=total-disc    #with discount
+if total>=1001:
+    print("Discount Applied:₹" ,disc)
+    print("Final Bill:₹" ,final2)
+else:
+    print("Final Bill:₹" ,final1)
+
